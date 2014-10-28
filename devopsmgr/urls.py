e710af71752d63f224860834e535645dbe3f946c
wangcheng <wangcheng@gzyouai.comgit>
2014-10-28 17:18:19 +0800

    简单测试

--- devopsmgr/urls.py
+++ devopsmgr/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from hxjh import views
 
 urlpatterns = [
     # Examples:
@@ -7,5 +8,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^hxjh/', include(hxjh.urls)),
+    url(r'^hxjh/name/', 'hxjh.views.hxjh_test'),
 ]
--- hxjh/views.py
+++ hxjh/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template import Template, Context
+from django.http import HttpResponse
 
 # Create your views here.
+def hxjh_test(request):
+    t = Template('<h1>hello, {{uname}}')
+    c = Context({'uname': 'alen'})
+    return HttpResponse(t.render(c))
