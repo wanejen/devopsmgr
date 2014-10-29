aad238a797c6b9508b8d5feb00251f07df000be0
wangcheng <wangcheng@gzyouai.comgit>
2014-10-29 14:25:14 +0800

    去掉idea目录的同步

--- hxjh/views.py
+++ hxjh/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def test(request):
-    t = Template('<h1>hello, {{uname}}')
-    c = Context({'uname': 'alen'})
-    return HttpResponse(t.render(c))
+#    t = Template('<h1>hello, {{uname}}')
+#    c = Context({'uname': 'alen'})
+    return HttpResponse("Hello, world")
